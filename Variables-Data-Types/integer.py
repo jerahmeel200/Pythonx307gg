1abc7687c43b38aa65d96c6f813f939f6235ed09
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-03-05 14:17:02 +0100

    feat:python variable and data types (int)

---
+++ Variables-Data-Types/integer.py
@@ -0,0 +1,12 @@
+# In Python, int is a built-in data type that represents integers (whole numbers). 
+# It is used to store numerical values without any decimal points.
+
+age = 25
+print(age) # 25
+
+print(type(age))  #output <class 'int'>
+
+
+x = int(3.5) # output 3
+
+
